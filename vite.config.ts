e5f762225e78daa86cb6bd90a343e6d693f872de
Mavludin <imevlud@gmail.com>
2023-08-22 16:27:20 +0300

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    eslint(),
     svgr({
       svgrOptions: {
         // svgr options
