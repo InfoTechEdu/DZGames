9bdb1f2123179a5ac5b4f228f9e7958128945d8a
Mavludin <imevlud@gmail.com>
2023-09-03 21:03:50 +0300

    add toggling

--- src/pages/Leaders/Leaders.tsx
+++ src/pages/Leaders/Leaders.tsx
@@ -1,42 +1,65 @@
-import styled from 'styled-components';
-import { MainTitle } from '../../components/MainTitle/MainTitle';
-import { MainCarousel } from '../../components/MainCarousel/MainCarousel';
-import { CAROUSEL_DATA } from '../../shared/slider';
-import { SubTitle } from '../../components/SubTitle/SubTitle';
-import { ReactComponent as SelectImg } from '../../assets/select.svg';
+import styled from "styled-components";
+import { MainTitle } from "../../components/MainTitle/MainTitle";
+import { MainCarousel } from "../../components/MainCarousel/MainCarousel";
+import { CAROUSEL_DATA } from "../../shared/slider";
+import { ReactComponent as SelectImg } from "../../assets/select.svg";
+import { useState } from "react";
 
 export const Leaders = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropDown = () => {
+    setShowDropdown((s) => !s);
+  };
+
   return (
     <LeaderContainer>
       <div>
-        <MainTitle text='Выбери игру' />
+        <MainTitle text="Выбери игру" />
         <div>
           <SelectGame>
-            Или найди по названию <SelectImg />
+            Или найди по названию
+            <button onClick={toggleDropDown}>
+              <SelectImg style={{ transform: showDropdown ? 'rotate(180deg)' : '' }} />
+            </button>
           </SelectGame>
-          <Options>
-            <Option>Юнга Мур и большая стройка котов-пиратов</Option>
-            <Option>Экология</Option>
-            <Option>Борьба Умов</Option>
-            <Option>Время Истории</Option>
-          </Options>
+          {showDropdown && (
+            <Options>
+              <Option>Юнга Мур и большая стройка котов-пиратов</Option>
+              <Option>Экология</Option>
+              <Option>Борьба Умов</Option>
+              <Option>Время Истории</Option>
+            </Options>
+          )}
         </div>
       </div>
       <div>
         <MainCarousel data={CAROUSEL_DATA} />
       </div>
+      {/* <div>
+        <SubTitle text='Таблица лидеров' />
+        <Table>
+          <tr>
+            <td>№</td>
+            <td>Имя</td>
+            <td>Класс</td>
+            <td>Очки</td>
+            <td>Игры</td>
+          </tr>
+        </Table>
+      </div> */}
     </LeaderContainer>
   );
 };
 
 const LeaderContainer = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  margin: '0 auto',
-  width: '100%',
-  maxWidth: '1224px',
-  paddingTop: '80px',
-  gap: '24px',
+  display: "flex",
+  flexDirection: "column",
+  margin: "0 auto",
+  width: "100%",
+  maxWidth: "1224px",
+  paddingTop: "80px",
+  gap: "24px",
 });
 
 const SelectGame = styled.div`
@@ -53,10 +76,14 @@ const SelectGame = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  cursor: pointer;
 `;
 
 const Options = styled.div`
+
+  position: absolute;
+  z-index: 10;
+  background-color: white;
+
   width: 392px;
   height: 172px;
   border-radius: 8px;
@@ -68,3 +95,7 @@ const Option = styled.div`
   color: #bda5ff;
   padding: 12px 16px;
 `;
+
+// const Table = styled.table`
+//   border: 1px solid black;
+// `;
