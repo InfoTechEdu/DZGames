c5fccbb6d0c32156d5f40ace31eb2e824651018e
Mavludin <imevlud@gmail.com>
2023-09-03 23:06:35 +0300

    fetch table data from server

--- src/pages/Leaders/Leaders.tsx
+++ src/pages/Leaders/Leaders.tsx
@@ -3,7 +3,7 @@ import { MainTitle } from "../../components/MainTitle/MainTitle";
 import { MainCarousel } from "../../components/MainCarousel/MainCarousel";
 import { CAROUSEL_DATA } from "../../shared/slider";
 import { ReactComponent as SelectImg } from "../../assets/select.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SubTitle } from "../../components/SubTitle/SubTitle";
 
 interface GameItem {
@@ -30,10 +30,28 @@ const gamesList: GameItem[] = [
   },
 ];
 
+const API_URL =
+  "https://us-central1-dzgames-12ad8.cloudfunctions.net/DownloadTop10Leaderboard?game=battleofminds";
+
+interface TableItem {
+  id: string;
+  name: string;
+  position: string;
+  profilePhoto: string;
+  progressData: {
+    points: number;
+  };
+  statistics: {
+    gamesPlayed: number;
+    winCount: number;
+  };
+}
+
 export const Leaders = () => {
   const [showDropdown, setShowDropdown] = useState(false);
-
   const [selectedGame, setSelectedGame] = useState<GameItem | null>(null);
+
+  const [tableData, setTableData] = useState<TableItem[] | null>(null);
 
   const toggleDropDown = () => {
     setShowDropdown((s) => !s);
@@ -43,6 +61,21 @@ export const Leaders = () => {
     setSelectedGame(game);
     setShowDropdown(false);
   };
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((data) => data.json())
+      .then((json) => {
+        const finalData = Object.entries(json).map(([id, item]) => {
+          return {
+            id,
+            ...item,
+          };
+        });
+
+        setTableData(finalData);
+      });
+  }, []);
 
   return (
     <LeaderContainer>
@@ -75,18 +108,74 @@ export const Leaders = () => {
       <div>
         <MainCarousel data={CAROUSEL_DATA} />
       </div>
-        <SubTitle text='Таблица лидеров' />
-        <TableWrapper imageSrc={new URL('../../assets/table-bg.png', import.meta.url).href}>
-        <Table>
-          <tr>
-            <td>№</td>
-            <td>Имя</td>
-            <td>Класс</td>
-            <td>Очки</td>
-            <td>Игры</td>
-          </tr>
-        </Table>
-      </TableWrapper>
+      {tableData && (
+        <>
+          <SubTitle text="Таблица лидеров" />
+          <TableWrapper
+            imageSrc={
+              new URL("../../assets/table-bg.png", import.meta.url).href
+            }
+          >
+            <Table>
+              <thead>
+                <tr>
+                  <th>№</th>
+                  <th>Имя</th>
+                  <th>Класс</th>
+                  <th>Очки</th>
+                  <th>Игры</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.map(
+                  (
+                    {
+                      id,
+                      name,
+                      position,
+                      profilePhoto,
+                      progressData: { points },
+                      statistics: { gamesPlayed },
+                    },
+                    index
+                  ) => {
+                    return (
+                      <tr key={id}>
+                        <td>{index + 1}</td>
+                        <td>
+                          <div>
+                            <ProfilePhoto src={profilePhoto} alt={name} />
+                            <span>{name}</span>
+                          </div>
+                        </td>
+                        <td>{position}</td>
+                        <td>{points}</td>
+                        <td>{gamesPlayed}</td>
+                      </tr>
+                    );
+                  }
+                )}
+              </tbody>
+            </Table>
+            <Table withBorder>
+              <tbody>
+                <tr>
+                  <td>-</td>
+                  <td>
+                    <div>
+                      <ProfilePhoto src={tableData[0]?.profilePhoto} alt='Я' />
+                      <span>Я</span>
+                    </div>
+                  </td>
+                  <td>1</td>
+                  <td>100</td>
+                  <td>1</td>
+                </tr>
+              </tbody>
+            </Table>
+          </TableWrapper>
+        </>
+      )}
     </LeaderContainer>
   );
 };
@@ -156,23 +245,72 @@ const Option = styled.button<{ isSelected: boolean }>`
   `}
 `;
 
-const TableWrapper = styled.div<{imageSrc: string}>`
+const TableWrapper = styled.div<{ imageSrc: string }>`
   width: 100%;
   background: ${({ imageSrc }) => `url(${imageSrc})`};
-  height: 786px;
-  background-color: #F7F7F8;
+  background-color: #f7f7f8;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+
+  padding: 30px 42px;
 `;
 
-const Table = styled.table`
-  border: 1px solid black;
-  border-collapse: collapse;
+const Table = styled.table<{withBorder?: boolean}>`
   width: 100%;
-  height: 100%;
 
-  & td, th {
-    border: 1px solid black
+  ${({ withBorder }) => withBorder && `
+    margin-top: 22px;
+    border-top: 2px solid black;
+  `};
+
+  & th {
+    font-size: 30px;
+    font-weight: 600;
+
+    text-align: left;
+  }
+
+  & td {
+    font-size: 30px;
+    font-weight: 300;
+
+    & div {
+      display: flex;
+      align-items: center;
+    }
+
+    & span {
+      margin-left: 10px;
+    }
+  }
+
+  & td {
+    padding-top: 22px;
+  }
+
+  & th:first-of-type, & tr td:first-of-type {
+    width: 5%
+  }
+
+  & th:nth-child(2), & tr td:nth-child(2) {
+    width: 50%
+  }
+
+  & th:nth-child(3), & tr td:nth-child(3) {
+    width: 15%
+  }
+
+  & th:nth-child(4), & tr td:nth-child(4) {
+    width: 15%
+  }
+
+  & th:last-of-type, & tr td:last-of-type {
+    width: 15%
   }
 `;
+
+const ProfilePhoto = styled.img`
+  width: 32px;
+  height: 32px;
+`
