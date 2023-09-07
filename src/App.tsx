8d9bf029df3df704917d93da27f5b10761747bd2
Mavludin <imevlud@gmail.com>
2023-09-07 14:57:56 +0300

    add scroll to top sctroller

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { AppRoutes } from './AppRoutes';
 import { Footer } from './components/Footer/Footer';
 import { FooterBg } from './components/FooterBg/FooterBg';
 import { Header } from './components/Header/Header';
+import { ScrollToTopController } from './containers/ScrollToTopController/ScrollToTopController';
 
 function App() {
   return (
     <>
       <div className='App'>
         <Header />
+        <ScrollToTopController />
         <AppRoutes />
         <Footer />
       </div>
